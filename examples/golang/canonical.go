ac01169329fc5ac7322623453fd2fde0c29b225b
Daniel Petersson <d.m.petersson@gmail.com>
2019-08-10 11:22:41 +0200

    first example in golang

---
+++ examples/golang/canonical.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	// TODO(dape): Canonical one line TODO entry
+
+	// TODO(dape): Canonical multiline TODO entry
+	//
+	// All remaining comment lines will be added as body of the issue.
+	// The first empty line will be omitted.
+
+	// This will just be ignored.
+}
